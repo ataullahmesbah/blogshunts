7b79a91709cba12c9c8326d5b7976a1252b91416
ataullahmesbah <mesbahataullah4@gmail.com>
2024-01-30 15:42:21 +0600

    banner section

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,10 +1,17 @@
+import Banner from "@/components/Home/Banner";
 import Image from "next/image";
+
+export const metadata = {
+  title: 'Hyascka - Your Digital Success Partner',
+  description: 'Discover the story behind Hyascka, your trusted partner for digital success. Learn about our journey, mission, and commitment to driving growth for businesses through innovative digital marketing strategies and expert web development services.',
+};
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
-    {/*  */}
+    <main className="">
+      <Banner />
+      {/*  */}
     </main>
   );
 }
--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,36 +1,62 @@
+import Image from "next/image";
 import Link from "next/link";
-import { FaChevronRight } from "react-icons/fa";
+import { FaArrowDown, FaChevronRight } from "react-icons/fa";
+import { HiArrowLongDown } from "react-icons/hi2";
+import bannerImg from '@/assets/hyascka-official-team-meet.jpeg'
 
 
 const Banner = () => {
     return (
-        <div className="bg-gray-100">
-            <div className="flex container mx-auto">
+        <div className="bg-gray-50">
+            <div className="relative flex flex-col lg:flex-row container mx-auto items-center justify-between pt-16 pb-16 px-2">
                 {/* left side text */}
-                <div className="space-y-3">
-                    <h1 className="lg:text-5xl md:text-4xl text-3xl font-semibold text-center lg:text-left mb-4">An Online Marketing Service That Helps You Grow Confidently</h1>
-                    <p className="text-lg lg:text-xl text-center lg:text-left mb-6">
+                <div className="lg:w-1/2 lg:pr-10 relative z-10 space-y-3 text-center lg:text-left">
+                    <h1 className="lg:text-5xl md:text-4xl text-3xl font-semibold mb-4">
+                        An Online Marketing Service That Helps You Grow Confidently
+                    </h1>
+                    <p className="text-lg lg:text-xl mb-6">
                         HYASCKA, your go-to partner for top-notch Digital Marketing and SEO Services.
                         Elevate your brand’s online visibility and drive meaningful results.
                     </p>
-                    <p className="text-lg lg:text-xl text-center lg:text-left mb-6">
+                    <p className="text-lg lg:text-xl mb-6">
                         Our Web Development expertise goes beyond coding; it’s about crafting exceptional digital experiences.
                         Explore our Web Developer Blog for insights into the latest trends and best practices.
                     </p>
-                    <p className="text-lg lg:text-xl text-center lg:text-left mb-6">
+                    <p className="text-lg lg:text-xl mb-6">
                         For E-Commerce ventures, we offer tailored solutions to boost your online store’s performance.
                         Trust HYASCKA to bring your digital aspirations to life.
                     </p>
 
-                    <Link className="flex items-center gap-3 bg-blue-500 hover:bg-blue-600 p-2 px-6 border-b-4 border-gray-400 rounded-md" href="/contactus" style={{ width: 'fit-content' }}>
-                        <button className="text-white font-semibold">
-                            Let’s Talk
-                        </button>
+                    <Link
+                        className="flex items-center gap-3 bg-blue-500 hover:bg-blue-600 p-2 px-6 border-b-4 border-gray-400 rounded-md"
+                        href="/contactus"
+                        style={{ width: 'fit-content' }}
+                    >
+                        <button className="text-white font-semibold">Let’s Talk</button>
                         <FaChevronRight className="text-white" />
                     </Link>
 
+                    <div className="flex items-center gap-2 pt-4">
+                        <div className="bg-blue-500 rounded-full p-2">
+                            <HiArrowLongDown className="text-white" />
+                        </div>
+                        <p className="font-semibold">Scroll For More</p>
+                    </div>
+
+                </div>
+
+                {/* cyan-100 circle */}
+                <div className="lg:w-1/2 mt-16 lg:mt-6 absolute top-0 lg:mx-[-40px]  left-0">
+                    <div className="w-56 h-56 bg-blue-50 rounded-full"></div>
+                </div>
+
+                {/* right side image */}
+                <div className="lg:w-1/2 mt-5 lg:mt-0 relative z-10">
+                    <Image className="rounded-md" src={bannerImg} alt="hyascka about us" width={600} height={381.5} placeholder="blur" />
                 </div>
             </div>
+
+
 
         </div>
     );
