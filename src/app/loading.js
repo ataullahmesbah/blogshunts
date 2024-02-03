cdddc51e2ffff486f0d5c74fc8451b215c1be197
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2024-02-04 00:55:19 +0600

    loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,11 +1,11 @@
-// import { PulseLoader } from 'react-spinners';
-// import 'react-spinners/css/pulseLoader.css';
+
 
 
 const loader = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            {/* <PulseLoader color="#36d7b7" size={15} margin={2} /> */}
+       
+
 
             <h1 className="text-3xl font-semibold">Loading ....</h1>
         </div>
